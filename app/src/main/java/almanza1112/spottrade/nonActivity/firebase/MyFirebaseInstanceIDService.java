1fbd322486695ceffeb6b869e27270fed8ce6353
almanza1112 <almanza1112@gmail.com>
2017-07-09 08:16:23 -0400

    added firebase

---
+++ app/src/main/java/almanza1112/spottrade/nonActivity/firebase/MyFirebaseInstanceIDService.java
@@ -0,0 +1,25 @@
+package almanza1112.spottrade.nonActivity.firebase;
+
+/**
+ * Created by almanza1112 on 7/9/17.
+ */
+
+import android.util.Log;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyAndroidFCMIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        //Get hold of the registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        //Log the token
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+    }
+    private void sendRegistrationToServer(String token) {
+        //Implement this method if you want to store the token on your server
+    }
+}
