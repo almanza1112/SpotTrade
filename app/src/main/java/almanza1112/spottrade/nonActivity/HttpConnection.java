14d5315cfa386944c759739ca11f5381f218bae2
almanza1112 <almanza1112@gmail.com>
2017-09-11 09:43:23 -0400

    changed url to heroku

--- app/src/main/java/almanza1112/spottrade/nonActivity/HttpConnection.java
+++ app/src/main/java/almanza1112/spottrade/nonActivity/HttpConnection.java
@@ -6,8 +6,7 @@ package almanza1112.spottrade.nonActivity;
 
 public class HttpConnection {
     public String htppConnectionURL(){
-        String localURL = "http://192.168.1.2";
-        String localPort = "3000";
-        return localURL + ":" + localPort;
+        //String url = "http://192.168.1.2:5000"; //for local
+        return "https://spottrade.herokuapp.com";
     }
 }
